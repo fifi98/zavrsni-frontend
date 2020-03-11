8e03c185dd9c8affb190cc0495b106c6cc42c82e
fifi98 <bel.filip@hotmail.com>
2020-03-11 15:11:55 +0100

    Adding and viewing menu items

--- src/components/MenuFood/AddForm.js
+++ src/components/MenuFood/AddForm.js
@@ -1,35 +1,58 @@
 import React, { useState } from "react";
+import API from "../../util/api";
 
-const AddForm = props => {
-  const [input, setInput] = useState({ title: "", description: "", price: "", categoryId: "" });
-
+const AddForm = ({ categories, input, handleSubmit, handleChange }) => {
   return (
-    <div class="alert alert-light border-bottom border-gray mb-4 pb-4" role="alert">
+    <div class="alert alert-light border-bottom border-gray mb-4 pb-4" role="alert" onSubmit={handleSubmit}>
       <form>
         <div className="row">
           <div className="col-6">
             <div class="form-group">
-              <label htmlFor="exampleInputEmail1">Title</label>
-              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+              <label htmlFor="exampleInputEmail1">Name</label>
+              <input
+                type="text"
+                className="form-control"
+                id="exampleInputEmail1"
+                name="name"
+                value={input.name}
+                onChange={handleChange}
+              />
             </div>
             <div class="form-group">
               <label htmlFor="exampleInputPassword1">Description</label>
-              <textarea class="form-control"></textarea>
+              <textarea className="form-control" name="description" value={input.description} onChange={handleChange} />
             </div>
           </div>
           <div className="col-6">
             <div class="form-group">
               <label htmlFor="exampleInputEmail1">Price</label>
-              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+              <input
+                type="text"
+                className="form-control"
+                name="price"
+                id="exampleInputEmail1"
+                value={input.price}
+                onChange={handleChange}
+              />
             </div>
             <div class="form-group">
-              <label htmlFor="exampleInputEmail1">Image</label>
-              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+              <label htmlFor="exampleInputEmail1">Category</label>
+              <select
+                class="custom-select"
+                name="categoryId"
+                id="inputGroupSelect01"
+                value={input.categoryId}
+                onChange={handleChange}
+              >
+                {categories.map(category => (
+                  <option value={category.category_id}>{category.category}</option>
+                ))}
+              </select>
             </div>
           </div>
         </div>
 
-        <button type="submit" class="btn btn-primary">
+        <button type="submit" className="btn btn-primary">
           Add
         </button>
       </form>
--- src/screens/restaurant/MenuFood.js
+++ src/screens/restaurant/MenuFood.js
@@ -12,10 +12,26 @@ const Tables = () => {
   const [searchKeyword, setSearchKeyword] = useState("");
   const [add, setAdd] = useState(false);
 
+  const [input, setInput] = useState({ name: "", description: "", price: "", categoryId: 0 });
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    API.post("/restaurant/4/menu/categories/" + input.categoryId + "/items", input).then(response => {
+      if (selectedCategory === parseInt(input.categoryId)) setMenuItems(response.data.data);
+      setAdd(false);
+      setInput({ name: "", description: "", price: "", categoryId: 0 });
+    });
+  };
+
+  const handleChange = event => {
+    setInput({ ...input, [event.target.name]: event.target.value });
+  };
+
   useEffect(() => {
     API.get("/restaurant/4/menu/categories").then(result => {
       setCategories(result.data.data);
       //Select the first category by default
+      setInput({ ...input, categoryId: result.data.data[0].category_id });
       setSelectedCategory(result.data.data[0].category_id);
     });
   }, []);
@@ -42,7 +58,9 @@ const Tables = () => {
   return (
     <MainContainer icon={faUtensils} handleAdd={handleAdd} title="Menu items">
       <div className="mt-2 pt-2">
-        {add && <AddForm />}
+        {add && (
+          <AddForm categories={categories} input={input} handleSubmit={handleSubmit} handleChange={handleChange} />
+        )}
         <div className="btn-group btn-group-toggle mb-3 w-100">
           {categories.map(category => (
             <label
