5a02dd0983ff9d200079d54d5601a64fbc1112d7
fifi98 <bel.filip@hotmail.com>
2020-06-01 15:36:40 +0200

    Change the design of item card when selected

--- src/components/user/FoodCard.js
+++ src/components/user/FoodCard.js
@@ -2,13 +2,13 @@ import React from "react";
 
 const FoodCard = ({ item, onClick, selected }) => {
   return (
-    <div className={`card mb-3 ${selected && "border-primary"} mt-2`} onClick={() => onClick(item)}>
+    <div className={`card food-card mb-3 ${selected ? "text-white bg-primary" : ""} mt-2`} onClick={() => onClick(item)}>
       <div className="row no-gutters">
         <div className="col-8 col-md-11">
           <div className="card-body">
             <h5 className="card-title">{item.name}</h5>
             <p className="card-text">
-              <small className="text-muted">{item.description}</small>
+              <small className={`${selected ? "text-white" : "text-muted"}`}>{item.description}</small>
             </p>
             <p className="card-text">{item.price}kn</p>
           </div>
